33ab830acc9f591f30e7773216110b33e381e278
Dameem Shahabaz <dz@skyslit.com>
2023-12-04 01:03:57 +0530

    refactor: updated ark

--- src/features/authentication/login.server.tsx
+++
@@ -1,8 +0,0 @@
-import { createBackendFunction, useFunctionContext } from '@skyslit/ark-backend';
-
-export default createBackendFunction(async function (firstName) {
-    const ctx = useFunctionContext(this);
-
-    
-    return `Hello ${firstName}`;
-});
